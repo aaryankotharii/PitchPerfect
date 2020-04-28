834b3f907756f2cb677638a2854be715fe8f1bab
aaryankotharii <aaryan.kothari@gmail.com>
2020-04-28 11:52:23 +0530

    viewController outlets added

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK:- Outlets
+    @IBOutlet var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func recordAudio(_ sender: UIButton) {
+        print("Recording started")
+        recordingLabel.text = "Now Recording"
+    }
+    
+    
+    @IBAction func stopRecording(_ sender: UIButton) {
+        print("Recording Stopped")
     }
 }
 
