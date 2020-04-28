144a68017f4015634034c8f131a5f29e92207fd4
aaryankotharii <aaryan.kothari@gmail.com>
2020-04-28 15:43:01 +0530

    play /stop audio function added

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -43,11 +43,27 @@ class PlaySoundsViewController: UIViewController {
     }
     
     @IBAction func playSoundForButton(_ sender: UIButton) {
-        
+        switch ButtonType(rawValue: sender.tag) {
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 1.5)
+        case .chipmunk:
+            playSound(pitch: 1000)
+        case .vader:
+            playSound(pitch: -1000)
+        case .echo:
+            playSound(echo: true)
+        case .reverb:
+            playSound(reverb : true)
+        case .none:
+            print("NONE")
+        }
+         configureUI(.notPlaying )
     }
     
     @IBAction func stopPressed(_ sender: UIButton) {
-        
+        stopAudio()
     }
     
 
